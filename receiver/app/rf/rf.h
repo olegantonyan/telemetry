a582e35b15df897e8bcdb7433fd751ac6b2fca1f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2017-06-24 18:25:02 +0200

    moving code around; playing with receiver

---
+++ receiver/app/rf/rf.h
@@ -0,0 +1,10 @@
+#ifndef __RF_H
+#define __RF_H
+
+#include "stm32f1xx_hal.h"
+
+bool rf_init(SPI_HandleTypeDef *spi, GPIO_TypeDef *gpio_port, uint16_t nsel_pin);
+bool rf_transmit(const uint8_t * data, size_t length);
+bool rf_receive(uint8_t *data, size_t length);
+
+#endif
